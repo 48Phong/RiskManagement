a532ce4ebe54ef31139523ccaf408b2160a0f837
Phong Le <caphesua0da@gmail.com>
2024-08-09 04:47:58 +0000

    Initial Commit

---
+++ app/demo/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'demo/test/integration/FirstJourney',
+		'demo/test/integration/pages/RisksList',
+		'demo/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('demo') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
